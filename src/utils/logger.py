ce52797133c3d508da4f744183d6cdca9473c572
prathamd69 <prathamd2710@gmail.com>
2026-06-19 03:08:03 +0530

    Logger configured.

---
+++ src/utils/logger.py
@@ -0,0 +1,34 @@
+import os
+import logging
+from logging import Logger
+
+logDir = os.path.join(os.getcwd(),"logs")
+os.makedirs(logDir, exist_ok=True)
+
+def configLogger(loggerName: str, fileName: str) -> Logger:
+
+    logger = logging.getLogger(loggerName)
+    logger.setLevel(logging.DEBUG)
+
+    # Prevent duplicate handlers if the logger is imported multiple times
+    if logger.hasHandlers():
+        return logger
+
+    logFormat = logging.Formatter(
+        "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+    )
+
+    filePath = os.path.join(logDir, fileName)
+    
+    fileHandler = logging.FileHandler(filePath)
+    fileHandler.setLevel(logging.DEBUG)
+    fileHandler.setFormatter(logFormat)
+
+    streamHandler = logging.StreamHandler()
+    streamHandler.setLevel(logging.DEBUG)
+    streamHandler.setFormatter(logFormat)
+
+    logger.addHandler(fileHandler)
+    logger.addHandler(streamHandler)
+
+    return logger
