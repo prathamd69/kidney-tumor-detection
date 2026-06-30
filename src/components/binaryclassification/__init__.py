0f975266ec7df4a678f16439b16c35cbef11b9f6
prathamd69 <prathamd2710@gmail.com>
2026-06-30 09:42:12 +0530

    deleted old utility files

--- src/components/binaryclassification/__init__.py
+++
@@ -1,3 +0,0 @@
-from .training_comps import training, testing, _get_model_binary
-
-__all__ = ['training', 'testing', '_get_model_binary']
--- src/components/multiclassification/__init__.py
+++
@@ -1,3 +0,0 @@
-from .training_comps import training, testing, _get_model_multi
-
-__all__ = ['training', 'testing', '_get_model_multi']
